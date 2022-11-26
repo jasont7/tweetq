8a9d9995ca71ec40ab629c055c5c47de13b66f43
Jason Thomo <jthomo@uvic.ca>
2022-11-25 16:48:06 -0800

    misc

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from './components/counter/counterSlice';
-import userRecentTweetsReducer from './components/user_recent_tweets/userRecentTweetsSlice'
+import counterReducer from '../components/counter/counterSlice';
+import userRecentTweetsReducer from '../components/user_recent_tweets/userRecentTweetsSlice'
 
 export const store = configureStore({
   reducer: {
