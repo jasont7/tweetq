674552089d3c75be26ddf9a0382e97e7455378b4
Jason Thomo <jthomo@uvic.ca>
2023-01-02 23:33:22 -0800

    misc

--- frontend/public/background.js
+++ frontend/public/background.js
@@ -1,18 +1,4 @@
 
-// Fires when the active tab in a window changes.
-// chrome.tabs.onActivated.addListener(async () => {
-//     let tab = await getCurrentTab();
-//     console.log(tab.url);
-// })
-
-// Fired when a tab is updated.
-// chrome.tabs.onUpdated.addListener(async () => {
-//     let tab = await getCurrentTab();
-//     chrome.tabs.sendMessage(tab.id, 
-//         {message: "tab_updated", url: tab.url}
-//     );
-// })
-
 chrome.webNavigation.onHistoryStateUpdated.addListener(async () => {
     let tab = await getCurrentTab();
     chrome.tabs.sendMessage(tab.id, 
