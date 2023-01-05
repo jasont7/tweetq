d59e13d46ac913a63f193db7ca295c025f9b6f4a
Jason Thomo <jthomo@uvic.ca>
2023-01-05 12:19:55 -0800

    invalid pages update

--- frontend/src/getUrlType.js
+++ frontend/src/getUrlType.js
@@ -1,20 +1,22 @@
 
 export default function getUrlType(url) {
   const urlObj = new URL(url);
-  const page = urlObj.pathname.substring(1);
+  const page = urlObj.pathname.substring(1).split('/')[0];
   
   const invalidPages = new Set([
+    'i',
     'explore',
     'notifications',
     'messages',
     'search',
+    'settings',
   ]);
 
   if (page == '' || page == 'home') {
     return {type: 'home'};
   }
 
-  if (!invalidPages.has(page) || page.includes('/status')) {
+  if (!invalidPages.has(page)) {
     const user = page.split('/')[0];
     return {type: 'user', user: user};
   }
